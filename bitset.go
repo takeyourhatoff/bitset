e0182d587e13ef39c1aa36b396c2f6c7e9648e20
Paddy Steed <jarktasaa@gmail.com>
2017-09-16 14:55:20 +0100

    Add bitset

---
+++ bitset.go
@@ -0,0 +1,215 @@
+// Package bitset provides Bitset, a compact and fast representation for a dense set of positive integer values.
+package bitset
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"math/bits"
+)
+
+const maxUint = 1<<bits.UintSize - 1
+
+// Bitset represents a set of positive integers. Memory usage is proportional to the largest integer in the Bitset.
+type Bitset struct {
+	s []uint
+}
+
+// Add adds the integer i to s. Add panics if i is less than zero.
+func (s *Bitset) Add(i int) {
+	if i < 0 {
+		panic("bitset: cannot add non-negative integer to set")
+	}
+	w, mask := idx(i)
+	for j := len(s.s); j <= w; j++ {
+		s.s = append(s.s, 0)
+	}
+	s.s[w] |= mask
+}
+
+// Remove removes the integer i from s, or does nothing if i is not already in s.
+func (s *Bitset) Remove(i int) {
+	if i < 0 {
+		// i < 0 cannot bit in set by definition.
+		return
+	}
+	w, mask := idx(i)
+	if w < len(s.s) {
+		s.s[w] &^= mask
+	}
+}
+
+// Test returns true if i is in s, false otherwise.
+func (s *Bitset) Test(i int) bool {
+	w, mask := idx(i)
+	if i < 0 || w >= len(s.s) {
+		return false
+	}
+	return s.s[w]&mask != 0
+}
+
+// Max returns the value of the maximum integer in s, or -1 if s is empty.
+func (s *Bitset) Max() int {
+	for n := len(s.s) - 1; n >= 0; n-- {
+		if s.s[n] == 0 {
+			continue
+		}
+		return bits.UintSize*(n+1) - bits.LeadingZeros(s.s[n]) - 1
+	}
+	return -1
+}
+
+// And removes integers in s which are not also in ss.
+func (s *Bitset) And(ss *Bitset) {
+	n := min(len(s.s), len(ss.s))
+	for i := 0; i < n; i++ {
+		s.s[i] &= ss.s[i]
+	}
+	s.s = s.s[:n]
+}
+
+// AndNot removes integers from s which are also in ss.
+func (s *Bitset) AndNot(ss *Bitset) {
+	n := min(len(s.s), len(ss.s))
+	for i := 0; i < n; i++ {
+		s.s[i] &^= ss.s[i]
+	}
+}
+
+// Or adds integers to s which are in ss.
+func (s *Bitset) Or(ss *Bitset) {
+	n := min(len(s.s), len(ss.s))
+	for i := 0; i < n; i++ {
+		s.s[i] |= ss.s[i]
+	}
+	if len(ss.s) > len(s.s) {
+		s.s = append(s.s, ss.s[len(s.s):]...)
+	}
+}
+
+// XOr adds integers to s which are in ss but not in s, and removes integers in s that are also in ss.
+func (s *Bitset) XOr(ss *Bitset) {
+	n := min(len(s.s), len(ss.s))
+	for i := 0; i < n; i++ {
+		s.s[i] ^= ss.s[i]
+	}
+	if len(ss.s) > len(s.s) {
+		s.s = append(s.s, ss.s[len(s.s):]...)
+	}
+}
+
+// Count returns the number of integers in s.
+func (s *Bitset) Count() int {
+	var n int
+	for i := range s.s {
+		n += bits.OnesCount(s.s[i])
+	}
+	return n
+}
+
+// NextAfter returns the smallest integer in s greater than or equal to i or -1 if no such integer exists.
+func (s *Bitset) NextAfter(i int) int {
+	if i < 0 {
+		// There can be no integers in s less than 0 by definition
+		i = 0
+	}
+	mask := uint(maxUint) << (uint(i) % bits.UintSize)
+	for j := i / bits.UintSize; j < len(s.s); j++ {
+		word := s.s[j] & mask
+		mask = maxUint
+		if word != 0 {
+			return j*bits.UintSize + bits.TrailingZeros(word)
+		}
+	}
+	return -1
+}
+
+// Copy returns a copy of s.
+func (s *Bitset) Copy() *Bitset {
+	n := len(s.s)
+	for n > 0 && s.s[n-1] == 0 {
+		n--
+	}
+	ss := new(Bitset)
+	ss.s = make([]uint, n)
+	copy(ss.s, s.s)
+	return ss
+}
+
+// String returns a string representation of s.
+func (s *Bitset) String() string {
+	var buf bytes.Buffer
+	buf.WriteRune('[')
+	first := true
+	for i := s.NextAfter(0); i >= 0; i = s.NextAfter(i + 1) {
+		if !first {
+			buf.WriteRune(' ')
+		}
+		fmt.Fprintf(&buf, "%d", i)
+		first = false
+	}
+	buf.WriteRune(']')
+	return buf.String()
+}
+
+// Bytes returns the set as a bitarray.
+// The most significant bit in each byte represents the smallest-index number.
+func (s *Bitset) Bytes() []byte {
+	const r = bits.UintSize / 8
+	b := make([]byte, len(s.s)*r)
+	b0 := b
+	for _, v := range s.s {
+		v = bits.Reverse(v)
+		switch bits.UintSize {
+		case 32:
+			binary.BigEndian.PutUint32(b, uint32(v))
+		case 64:
+			binary.BigEndian.PutUint64(b, uint64(v))
+		default:
+			panic("uint is not 32 or 64 bits long")
+		}
+		b = b[r:]
+	}
+	for len(b0) > 0 && b0[len(b0)-1] == 0 {
+		b0 = b0[:len(b0)-1]
+	}
+	return b0
+}
+
+// FromBytes sets s to the value of data interpreted as a bitarray in the same format as produced by Bytes..
+func (s *Bitset) FromBytes(data []byte) *Bitset {
+	const r = bits.UintSize / 8
+	if len(data) == 0 {
+		s.s = nil
+	}
+	for len(data)%r != 0 {
+		data = append(data, 0)
+	}
+	s.s = make([]uint, len(data)/r)
+	for i := range s.s {
+		switch bits.UintSize {
+		case 32:
+			s.s[i] = uint(binary.BigEndian.Uint32(data))
+		case 64:
+			s.s[i] = uint(binary.BigEndian.Uint64(data))
+		default:
+			panic("uint is not 32 or 64 bits long")
+		}
+		s.s[i] = bits.Reverse(s.s[i])
+		data = data[r:]
+	}
+	return s
+}
+
+func idx(i int) (w int, mask uint) {
+	w = i / bits.UintSize
+	mask = 1 << (uint(i) % bits.UintSize)
+	return
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
---
+++ bitset_test.go
@@ -0,0 +1,363 @@
+package bitset
+
+import (
+	"bytes"
+	"fmt"
+	"math/bits"
+	"math/rand"
+	"reflect"
+	"runtime"
+	"sort"
+	"testing"
+	"testing/quick"
+)
+
+// NextAfter can be used to iterate over the elements of the set.
+func ExampleBitset_NextAfter() {
+	s := new(Bitset)
+	s.Add(2)
+	s.Add(42)
+	s.Add(13)
+	for i := s.NextAfter(0); i >= 0; i = s.NextAfter(i + 1) {
+		fmt.Println(i)
+	}
+	// Output:
+	// 2
+	// 13
+	// 42
+}
+
+func ExampleBitset_String() {
+	s := new(Bitset)
+	s.Add(2)
+	s.Add(42)
+	s.Add(13)
+	fmt.Println(s)
+	// Output: [2 13 42]
+}
+
+func ExampleBitset_Bytes() {
+	s := new(Bitset)
+	s.Add(0)
+	s.Add(3)
+	s.Add(8)
+	s.Add(10)
+	b := s.Bytes()
+	fmt.Printf("%b %b", b[0], b[1])
+	// Output: 10010000 10100000
+}
+
+func TestAdd_Panic(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Error("b.Add(-1) did not panic")
+		} else if err, ok := r.(runtime.Error); ok {
+			t.Error(err)
+		}
+	}()
+	new(Bitset).Add(-1)
+}
+
+func TestAddAndTest(t *testing.T) {
+	f := func(l ascendingInts) bool {
+		b := new(Bitset)
+		for _, i := range l {
+			b.Add(i)
+		}
+		min := -10
+		max := 10
+		if len(l) > 0 {
+			max += l[len(l)-1]
+		}
+		for i := min; i < max; i++ {
+			if v := b.Test(i); v != in(i, l) {
+				t.Logf("b.Test(%d) = %v, expected %v", i, v, in(i, l))
+				return false
+			}
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	f := func(l0, l1 ascendingInts) bool {
+		b := new(Bitset)
+		for _, i := range l0 {
+			b.Add(i)
+		}
+		// set l1 to be a subset of l0
+		l1 = bitwiseF(func(p, q bool) bool { return p && q }, l0, l1)
+		// remove that subset
+		for _, i := range l1 {
+			b.Remove(i)
+		}
+		// set l0 to be l0 - l1
+		l0 = bitwiseF(func(p, q bool) bool { return p && !q }, l0, l1)
+		min := -10
+		max := 10
+		if len(l0) > 0 {
+			max += l0[len(l0)-1]
+		}
+		for i := min; i < max; i++ {
+			if v := b.Test(i); v != in(i, l0) {
+				t.Logf("b.Test(%d) = %v, expected %v", i, v, in(i, l0))
+				return false
+			}
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCopy(t *testing.T) {
+	f0 := func(l ascendingInts) string {
+		b := new(Bitset)
+		for _, i := range l {
+			b.Add(i)
+		}
+		// Remove the last half to test Copy's trailing zero logic
+		lr := l[len(l)/2:]
+		for _, i := range lr {
+			b.Remove(i)
+		}
+		return b.String()
+	}
+	f1 := func(l ascendingInts) string {
+		b := new(Bitset)
+		for _, i := range l {
+			b.Add(i)
+		}
+		lr := l[len(l)/2:]
+		for _, i := range lr {
+			b.Remove(i)
+		}
+		return b.Copy().String()
+	}
+	if err := quick.CheckEqual(f0, f1, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMax(t *testing.T) {
+	f := func(l ascendingInts) bool {
+		b := new(Bitset)
+		for _, i := range l {
+			b.Add(i)
+		}
+		// remove last half to test Max's trailing zero logic
+		l0, l1 := l[:len(l)/2], l[len(l)/2:]
+		for _, i := range l1 {
+			b.Remove(i)
+		}
+		max := b.Max()
+		if len(l0) == 0 {
+			if max == -1 {
+				return true
+			} else {
+				t.Logf("b.Max() = %v, expected -1", max)
+				return false
+			}
+		}
+		if lMax := l0[len(l0)-1]; max != lMax {
+			t.Logf("b.Max() = %v, expected %v", max, lMax)
+			return false
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCount(t *testing.T) {
+	f := func(l ascendingInts) bool {
+		b := new(Bitset)
+		for _, i := range l {
+			b.Add(i)
+		}
+		if count := b.Count(); count != len(l) {
+			t.Logf("b.Count() = %d, expected %d", count, len(l))
+			return false
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestNextAfter(t *testing.T) {
+	f := func(l ascendingInts) bool {
+		b := new(Bitset)
+		for _, i := range l {
+			b.Add(i)
+		}
+		var n int
+		var oldi int
+		for i := b.NextAfter(0); i >= 0; i = b.NextAfter(i + 1) {
+			if l[n] != i {
+				t.Logf("b.NextAfter(%d) = %d, expected %d", oldi, i, l[n])
+				return false
+			}
+			oldi = i
+			n++
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBytes(t *testing.T) {
+	f := func(data0 []byte) bool {
+		// Get rid of trailing zero bytes
+		for len(data0) > 0 && data0[len(data0)-1] == 0 {
+			data0 = data0[:len(data0)-1]
+		}
+		b := new(Bitset)
+		b.FromBytes(data0)
+		if data1 := b.Bytes(); bytes.Equal(data0, data1) == false {
+			t.Logf("b.Bytes() = %v, expected %v", data1, data0)
+			return false
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestString(t *testing.T) {
+	f := func(l ascendingInts) bool {
+		b := new(Bitset)
+		for _, i := range l {
+			b.Add(i)
+		}
+		if s := b.String(); s != fmt.Sprintf("%v", l) {
+			t.Logf("b.String() = %v, wanted %v", s, l)
+			return false
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBitwise(t *testing.T) {
+	for _, v := range []struct {
+		op string
+		lf func(p, q bool) bool
+		bf func(s0, s1 *Bitset)
+	}{
+		{
+			"and",
+			func(p, q bool) bool {
+				return p && q
+			},
+			func(s0, s1 *Bitset) {
+				s0.And(s1)
+			},
+		},
+		{
+			"and not",
+			func(p, q bool) bool {
+				return p && !q
+			},
+			func(s0, s1 *Bitset) {
+				s0.AndNot(s1)
+			},
+		},
+		{
+			"or",
+			func(p, q bool) bool {
+				return p || q
+			},
+			func(s0, s1 *Bitset) {
+				s0.Or(s1)
+			},
+		},
+		{
+			"xor",
+			func(p, q bool) bool {
+				return p != q
+			},
+			func(s0, s1 *Bitset) {
+				s0.XOr(s1)
+			},
+		},
+	} {
+		f0 := func(l0, l1 ascendingInts) string {
+			b0 := new(Bitset)
+			for _, i := range l0 {
+				b0.Add(i)
+			}
+			b1 := new(Bitset)
+			for _, i := range l1 {
+				b1.Add(i)
+			}
+			v.bf(b0, b1)
+			return b0.String()
+		}
+		f1 := func(l0, l1 ascendingInts) string {
+			lx := bitwiseF(v.lf, l0, l1)
+			return fmt.Sprint(lx)
+		}
+		if err := quick.CheckEqual(f0, f1, nil); err != nil {
+			t.Errorf("Op: %s\n%v", v.op, err)
+		}
+	}
+}
+
+func bitwiseF(f func(p, q bool) bool, l0, l1 []int) []int {
+	var x []int
+	lim := max(l0, l1)
+	for i := 0; i <= lim; i++ {
+		inl0, inl1 := in(i, l0), in(i, l1)
+		if f(inl0, inl1) {
+			x = append(x, i)
+		}
+	}
+	return x
+}
+
+func in(x int, xs []int) bool {
+	i := sort.SearchInts(xs, x)
+	return i < len(xs) && xs[i] == x
+}
+
+func max(a, b []int) int {
+	if len(a) == 0 {
+		if len(b) == 0 {
+			return 0
+		}
+		return b[len(b)-1]
+	}
+	if len(b) == 0 {
+		return a[len(a)-1]
+	}
+	ai, bi := a[len(a)-1], b[len(b)-1]
+	if ai > bi {
+		return ai
+	}
+	return bi
+}
+
+type ascendingInts []int
+
+func (l ascendingInts) Generate(rand *rand.Rand, size int) reflect.Value {
+	l = make([]int, rand.Intn(size))
+	var x int
+	for i := range l {
+		x += rand.Intn(bits.UintSize+1) + 1
+		l[i] = x
+	}
+	return reflect.ValueOf(l)
+}
